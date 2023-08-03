a4b34001486c9b9c3932d8487d7ef6481772e106
DEVeloper <devmody1268@gmail.com>
2023-08-03 14:56:36 -0400

    askad

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,6 @@ module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
     extend: {
-      colors:{
-        powderBlue: "#98C1D9",
-        lightCyan:"#E0FBFC",
-        greyBlue: "#3D5A80",
-        skyBlue: "#B9EBFF"
-      },
       fontFamily: {
         body: 'Poppins'
       }
